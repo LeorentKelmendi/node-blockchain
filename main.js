d162ec820f3000a192fe425db161d90e42cc434d
Leo <leorent@provideit.se>
2019-02-12 23:42:53 +0100

    create blockchains with sha256

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index+this.previeousHash+this.timestamp+JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2019", "Test block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let nodeCoin = new Blockchain();
+nodeCoin.addBlock(new Block(1, '02/02/2019', {amount:4}));
+nodeCoin.addBlock(new Block(2, '02/04/2019', {amount:10}));
+
+console.log(JSON.stringify(nodeCoin, null , 4));
